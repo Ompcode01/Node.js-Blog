962e31e674cc1a1e9ca8c8acca8e7a1e14007790
Ompcode01 <omprakashpandey1517@gmail.com>
2026-01-16 14:13:38 +0530

    Setup, Folders, etc done.

---
+++ app.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+const express = require('express');
+const expressLayout = require('express-ejs-layout');
+
+const app = express();
+const PORT = 5000 || process.env.PORT;
+
+app.use(express.static('public'));  // To store Static files like images, etc 
+
+//Templating Engine
+app.use(expressLayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+app.use('/', require("./server/routes/main"));  // We are putting every routes at different position. 
+
+app.listen(PORT, ()=> {
+    console.log(`App listening on port ${PORT}`);
+});
